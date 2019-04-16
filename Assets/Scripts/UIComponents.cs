ca7477b8b051240cc4f0dfc9913162c6a1d0ff17
aquelamenina <ingridnunest@gmail.com>
2019-04-16 16:20:04 -0300

    Criando a classe UIComponents e fazendo as referencias

---
+++ Assets/Scripts/UIComponents.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine.UI;
+using UnityEngine;
+
+[Serializable]
+public class UIComponents 
+{
+    [Serializable]
+    public class Hud {
+
+       [Header("Text")]
+       public Text txtCoinCount;
+
+       public Text txtLifeCount;
+
+       [Header("Other")]
+       public GameObject panelHud; 
+    }
+
+    public Hud hud; 
+}
